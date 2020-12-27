89a134a0f6a0cf23b9e4b716a8736a0b17dc2361
Sirvess <joe.slask@gmail.com>
2020-12-27 18:17:40 +0100

    Add tsconfig and rollupconfig

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from "@rollup/plugin-typescript";
+import html from "@rollup/plugin-html";
+import replace from "@rollup/plugin-replace";
+
+export default {
+    input: "./src/index.tsx",
+    output: {
+        file: "./dist/bundle.js",
+    },
+    plugins: [
+        resolve({browser:true}),
+        commonjs({
+            transformMixedEsModules: true
+        }),
+        typescript(),
+        html(),
+        replace({'process.env.NODE_ENV': JSON.stringify('DEV')})
+    ]
+};
